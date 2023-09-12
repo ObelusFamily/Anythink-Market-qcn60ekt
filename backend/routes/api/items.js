5e2b9cba1c78095bce39099aa54ba8728cd2d670
br-user <118740903+br-user@users.noreply.github.com>
2023-09-12 05:50:29 +0000

    fix

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -89,6 +89,7 @@ router.get("/", auth.optional, function(req, res, next) {
           items: await Promise.all(
             items.map(async function(item) {
               item.seller = await User.findById(item.seller);
+              item.seller.isVerified = false;
               return item.toJSONFor(user);
             })
           ),
